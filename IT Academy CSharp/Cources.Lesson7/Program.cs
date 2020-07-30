e052736fb2ffb8fa84bd33083ed0f7e9210d2ed9
AlekseyZabrodin2 <66990966+AlekseyZabrodin2@users.noreply.github.com>
2020-07-30 16:55:29 +0300

    Begin

--- IT Academy CSharp/Cources.Lesson7/Program.cs
+++ IT Academy CSharp/Cources.Lesson7/Program.cs
@@ -6,7 +6,32 @@ namespace Cources.Lesson7
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Student student1 = new Student("Андрейка", "Тимохович");
+      Student student2 = new Student("Виталька", "Тимохович");
+      Student student3 = new Student("Валли", "Тимохович");
+      Student student4 = new Student("Козел", "Тимохович");
+
+      student1.PrintStudentName();
+      student2.PrintStudentName();
+      student3.PrintStudentName();
+      student4.PrintStudentName();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      Console.ReadLine();
     }
   }
 }
---
+++ IT Academy CSharp/Cources.Lesson7/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cources.Lesson7
+{
+  class Student
+  {
+    public string FirstName { get; set; }
+    public double AverageMarc { get; set; }
+    public string SecendName { get; set; }
+
+    int[] mark = new int[4];
+    Random random = new Random();
+
+    public Student(string fName, string sName)
+    {
+      FirstName = fName;
+      SecendName = sName;
+      
+      for (int i = 0; i < mark.Length; i++)
+      {
+        mark[i] = random.Next(0, 11);
+        AverageMarc += mark[i];
+      }
+      AverageMarc = AverageMarc / mark.Length;
+    }
+
+    public void PrintStudentName()
+    {
+      Console.WriteLine($"student {FirstName} {SecendName}");
+    }
+
+
+  }
+}
