963057c1022adc396970465bc591200c476063ab
Aleksey Zabrodin <rediskal50@gmail.com>
2020-08-06 15:17:42 +0300

    Add HomeTask8, class MyString, Method CharIndexOf and CharContains

---
+++ IT Academy CSharp/Cources.HomeTask8/MyString.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Dynamic;
+using System.Linq;
+
+
+namespace Cources.HomeTask8
+{
+    public static class MyString
+    {
+        public static bool CharContains(this char[] charConteyner, char value)
+        {
+            bool result = false;
+
+            foreach (var item in charConteyner)
+            {
+                if (item == value)
+                {
+                    result = true;
+                }
+            }
+            //Console.WriteLine("Все норм");
+            return result;
+        }
+
+        public static int CharIndexOf(this char[] charConteyner, int ind ) 
+        {
+            
+            for (int i = 0; i < charConteyner.Length; i++)
+            {
+                if (charConteyner[i] == ind)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        
+        }
+
+
+    } 
+
+
+
+
+    
+}
---
+++ IT Academy CSharp/Cources.HomeTask8/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Linq;
+
+namespace Cources.HomeTask8
+{
+    public class Program
+    {
+
+
+        public static void Main(string[] arg)
+        {
+            var charConteyner = new char[16] {'Y', 'c', 'h', 'e', 'b', 'n', 'o', 'e', '_', 'z', 'a', 'd', 'a', 'n', 'i', 'e'};
+
+
+            var ind = charConteyner.CharIndexOf('z') ;
+            var result = charConteyner.CharContains('c');
+
+
+            Console.WriteLine(ind);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
