53a045969f768021f6b327f386e6ad189fb6f540
Zabrodin Alexey <rediskal50@gmail.com>
2020-08-27 22:01:15 +0300

    Add Cources.Lesson13

---
+++ IT Academy CSharp/Cources.Lesson13/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Cources.Lesson13
+{
+  public class Student { }
+  public class Group { }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // divide by zero
+      try
+      {
+        var a = 0;
+        var result = 2 / a;
+      }
+      catch (DivideByZeroException exc)
+      {
+        Console.WriteLine("Возникло исключение " + exc.Message);
+      }
+
+      // index out of renge
+      try
+      {
+        string[] arrey = new string[] { "что нибудь" };
+        var item = arrey[2];
+      }
+      catch (IndexOutOfRangeException exc)
+      {
+        Console.WriteLine("Возникло исключение " + exc.Message);
+      }
+
+      // invalid cast 
+      try
+      {
+        object student = new Student();
+        Group group = (Group)student;
+      }
+      catch (InvalidCastException exc)
+      {
+        Console.WriteLine("Возникло исключение " + exc.Message);
+      }
+
+      // null reference
+      try
+      {
+        Student student1 = null;
+        student1.ToString();
+      }
+      catch (NullReferenceException exc)
+      {
+        Console.WriteLine("Возникло исключение " + exc.Message);
+      }
+
+
+
+      try
+      {
+        var a = Convert.ToInt32(Console.ReadLine());
+        var result = 2 / a;
+        string[] arrey = new string[] { "что нибудь" };
+        var item = arrey[Convert.ToInt32(Console.ReadLine())];
+      }
+      catch (DivideByZeroException exc)
+      {
+        Console.WriteLine("Возникло исключение " + exc.Message);
+      }
+      
+      catch (IndexOutOfRangeException exc)
+      {
+        Console.WriteLine("Возникло исключение " + exc.Message);
+      }
+
+      Console.ReadLine();
+
+    }
+  }
+}
