0f60bef0f6fc8e33309a6c1f9963af92783ae2a6
Aleksey Zabrodin <rediskal50@gmail.com>
2020-08-27 16:36:55 +0300

    Add HomeTask 11

---
+++ IT Academy CSharp/Cources.HomeTask11/Professor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lesson11
+{
+    class Professor : Person
+    {
+
+        private string _name;
+
+        public override string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                else
+                    _name = value;
+            }
+        }
+
+        public Professor(string name, string surname, Faculty faculty) : base(name, surname)
+        {
+            Faculty = faculty;
+        }
+
+        public override bool Equals(Person person)
+        {
+            if (this.Faculty == person.Faculty)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void PrintName()
+        {
+            Console.WriteLine($"Prof. {Name} {Surame}");
+        }
+    }
+}
---
+++ IT Academy CSharp/Cources.HomeTask11/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson11
+{
+    class Student : Person
+    {
+        public Student(string name, string surname, Faculty faculty):base(name, surname)
+        {
+            Faculty = faculty;
+        }
+
+        private string _name;
+
+        public override string Name 
+        {
+            get 
+            {
+                return _name;               
+            }
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                else
+                    _name = value;
+            }
+        }
+
+        public override bool Equals(Person person)
+        {
+            if (this.Faculty == person.Faculty)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void PrintName()
+        {
+            Console.WriteLine($"Junior {Name} {Surame}");
+        }
+    }
+}
