dc53e043ff15777efb708cb649df7381c5f1c5f4
Zabrodin Alexey <rediskal50@gmail.com>
2020-09-07 23:09:28 +0300

    [+] Add Cources Lessons 15 and 16

---
+++ IT Academy CSharp/Cources.Lesson15/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp26
+{
+  public enum Priority
+  {
+    HIGH,
+    NORMAL
+  }
+
+  public class MyQueue<T>
+  {
+    private List<T> list = new List<T>();
+    public void Enqueue(T item, Priority priority = Priority.NORMAL)
+    {
+      if (priority == Priority.HIGH)
+      {
+        list.Insert(0, item);
+      }
+      else
+        list.Add(item);
+    }
+  public T Dequeue()
+    {
+      if (typeof(T) == typeof(int))
+      {
+        var itemInt = list.Cast<int>().FirstOrDefault(i => i > 100);
+        var index = list.Cast<int>().ToList().IndexOf(itemInt);
+        var item = list[index];
+        list.RemoveAt(index);
+        return item;
+      }
+      else
+      {
+        var item = list[0];
+        list.Remove(item);
+        return item;
+      }
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      MyQueue<int> myQueue = new MyQueue<int>();
+      myQueue.Enqueue(1);
+      myQueue.Enqueue(2);
+      myQueue.Enqueue(3);
+
+      var item = myQueue.Dequeue();
+
+      Console.WriteLine(item);
+      item = myQueue.Dequeue();
+      Console.WriteLine(item);
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ IT Academy CSharp/Cources.Lesson16/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson16
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //int[] numbers = new int[] { 12, 34, 2, 19, 10, 123, 54, 4 };
+      int[] numbers = new int[] { 8, 2, 2, 4, 3, 6, 3 };
+
+      //List<int> numbersUnic = new List<int>();
+
+      //foreach(int number in numbers)
+      //{
+      //    if(!numbersUnic.Contains(number))
+      //    {
+      //        numbersUnic.Add(number);
+      //    }
+
+      //}
+      var str = "А роза упала на лапу азора".Distinct();
+      List<int> numbersUnic = numbers.Distinct().ToList();
+
+      for (int i = 0; i < numbersUnic.Count; i++)
+      {
+        Console.Write(numbersUnic[i] + " - ");
+        for (int j = 0; j < numbersUnic.Count; j++)
+        {
+          if (i == j)
+          {
+            continue;
+          }
+          if (numbersUnic[i] % numbersUnic[j] == 0)
+          {
+
+            Console.Write(numbersUnic[j] + " ");
+
+          }
+
+        }
+        Console.WriteLine();
+
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
